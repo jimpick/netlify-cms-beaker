db3a27b9189f2dbe4ef973e91b64e04b22a973db
Jim Pick <jim@backblend.com>
2017-09-23 16:54:58 -0700

    Starting work on a Beaker Browser backend
    Use `npm run start:beaker` to compile the example to the ./dist
    directory, and then use `dat create` and `dat share` from there
    to generate a Dat archive that can be loaded in Beaker Browser.

---
+++ src/backends/beaker/AuthenticationPage.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Input from "react-toolbox/lib/input";
+import Button from "react-toolbox/lib/button";
+import { Card, Icon } from "../../components/UI";
+import logo from "../git-gateway/netlify_logo.svg";
+import styles from "../git-gateway/AuthenticationPage.css";
+
+export default class AuthenticationPage extends React.Component {
+  static propTypes = {
+    onLogin: PropTypes.func.isRequired,
+  };
+
+  state = { email: '' };
+
+  handleLogin = (e) => {
+    e.preventDefault();
+    this.props.onLogin(this.state);
+  };
+
+  handleEmailChange = (value) => {
+    this.setState({ email: value });
+  };
+
+  render() {
+    return (<section className={styles.root}>
+      <Card className={styles.card}>
+        <img src={logo} width={100} role="presentation" />
+        <p className={styles.message}>This is a demo, enter your email to start</p>
+        <Input
+          type="text"
+          label="Email"
+          name="email"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
+        <Button
+          className={styles.button}
+          raised
+          onClick={this.handleLogin}
+        >
+          <Icon type="login" /> Login
+        </Button>
+      </Card>
+    </section>);
+  }
+}
